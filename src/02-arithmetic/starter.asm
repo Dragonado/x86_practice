8b82d4ab22ecb09b91874cc36f1d23ed2254f92a
Dragonado <29285950+Dragonado@users.noreply.github.com>
2026-09-12 02:59:25 -0400

    add solution 2

--- src/02-arithmetic/starter.asm
+++ src/02-arithmetic/starter.asm
@@ -1,8 +1,10 @@
 global _start
 
 section .data
-    message      db   'TODO: implement this module', 10
+    message      db   'arithmetic result: '
     message_len  equ  $ - message
+    zero         db   '0'
+    buf          db   '00'
 
 section .text
 
@@ -12,6 +14,29 @@ _start:
     mov ecx, message
     mov edx, message_len
     int 0x80
+    ; ------------------------
+    mov eax, 1                          ; input 1
+    mov ebx, 20                         ; input 2
+    xor ecx, ecx                        ; result
+    add ecx, eax                        ; 1
+    add ecx, ebx                        ; 21
+    add ecx, ecx                        ; 42
+
+    xor edx, edx                        ; setting to 0
+    mov eax, ecx                        ; EDX:EAX is now 42
+    mov ecx, 10                         ; divisor is 10
+    div ecx                             ; divide, eax has quotient, ecx has remainder
+
+    add eax, [zero]                     ; add '0' to make it ASCII
+    add edx, [zero]                     ; add '0' to make it ASCII
+    mov [buf], eax                      ; ovewrite 0 with 4
+    mov [buf + 1], edx                  ; ovewrite 0 with 2
+    mov ecx, buf                        ; buffer pointer to ecx
+    mov edx, 2                          ; length of buffer
+    mov eax, 4                          ; write syscall
+    mov ebx, 1                          ; stdout
+    int 0x80                            ; interrupt
+    ; ------------------------
     mov eax, 1                          ; exit
     xor ebx, ebx
     int 0x80
