3e412326be2548944318e9da74724df6ad59b9a8
Dragonado <29285950+Dragonado@users.noreply.github.com>
2026-09-12 19:11:14 -0400

    Add solution for 4 control flow

--- src/04-control-flow-1/starter.asm
+++ src/04-control-flow-1/starter.asm
@@ -1,17 +1,58 @@
 global _start
 
 section .data
-    message      db   'TODO: implement this module', 10
-    message_len  equ  $ - message
+    buffer              db   '0000'
+    equal_string        db   'branch: equal'
+    equal_string_len    equ  $ - equal_string
+    unequal_string      db   'branch: unequal'
+    unequal_string_len  equ  $ - unequal_string
 
 section .text
 
 _start:
+    ; ---------------- 
+    ; READ FROM INPUT 
+    ; ---------------- 
+    mov eax, 3                          ; read
+    mov ebx, 0                          ; stdin
+    mov ecx, buffer                     ; input buffer
+    mov edx, 3                          ; read 3 bytes
+    int 0x80
+
+    ; ---------------- 
+    ; LOAD DATA FROM INPUT BUFFER 
+    ; ---------------- 
+    mov al, [buffer]
+    mov bl, [buffer + 2]
+
+    ; ---------------- 
+    ; Conditional jump 
+    ; ---------------- 
+    cmp al, bl
+    jne unequal
+
+    ; ---------------- 
+    ; PRINT EQUAL AND EXIT 
+    ; ---------------- 
     mov eax, 4                          ; write
     mov ebx, 1                          ; stdout
-    mov ecx, message
-    mov edx, message_len
+    mov ecx, equal_string               ; 
+    mov edx, equal_string_len           ; 
     int 0x80
     mov eax, 1                          ; exit
     xor ebx, ebx
     int 0x80
+
+    ; ---------------- 
+    ; PRINT UNEQUAL AND EXIT 
+    ; ---------------- 
+
+    unequal:
+        mov eax, 4                      ; write
+        mov ebx, 1                      ; stdout
+        mov ecx, unequal_string         ; 
+        mov edx, unequal_string_len     ; 
+        int 0x80
+        mov eax, 1                      ; exit
+        xor ebx, ebx
+        int 0x80
