4fc38630474e6bc8caf9e7bd8a34db34fcf675a4
Dragonado <29285950+Dragonado@users.noreply.github.com>
2026-09-12 19:23:07 -0400

    Add solution 5 control flow 2

--- src/05-control-flow-2/starter.asm
+++ src/05-control-flow-2/starter.asm
@@ -1,17 +1,46 @@
 global _start
 
 section .data
-    message      db   'TODO: implement this module', 10
+    message      db   'loop sum: '
+    answer       db   '00'
     message_len  equ  $ - message
 
 section .text
 
 _start:
-    mov eax, 4                          ; write
-    mov ebx, 1                          ; stdout
-    mov ecx, message
-    mov edx, message_len
-    int 0x80
-    mov eax, 1                          ; exit
-    xor ebx, ebx
-    int 0x80
+    xor eax, eax
+    mov ebx, 1
+    ; ---------------- 
+    ; LOOP FOR 10 ITERATIONS
+    ; ---------------- 
+
+    sum_loop:
+        add eax, ebx
+        add ebx, 1
+        cmp ebx, 11
+        jne sum_loop
+    ; ---------------- 
+    ; WRITE TO BUFFER 
+    ; ---------------- 
+
+    after_loop:
+        xor edx, edx
+        mov ecx, 10
+        div ecx
+        add edx, '0'
+        add eax, '0'
+        mov [answer], al
+        mov [answer + 1], dl
+    ; ---------------- 
+    ; PRINT AND EXIT 
+    ; ---------------- 
+
+    print:
+        mov eax, 4                      ; write
+        mov ebx, 1                      ; stdout
+        mov ecx, message
+        mov edx, message_len
+        int 0x80
+        mov eax, 1                      ; exit
+        xor ebx, ebx
+        int 0x80
