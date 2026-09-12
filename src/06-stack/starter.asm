08a6a274c40ecab81c5485290d07e5a893cdaddb
Dragonado <29285950+Dragonado@users.noreply.github.com>
2026-09-12 19:36:53 -0400

    Add stack solution 5

--- src/06-stack/starter.asm
+++ src/06-stack/starter.asm
@@ -1,12 +1,45 @@
 global _start
 
 section .data
-    message      db   'TODO: implement this module', 10
+    message      db   'stack value: '
+    answer       db   '00'
     message_len  equ  $ - message
 
 section .text
 
 _start:
+    ; ---------------- 
+    ; PUSH 23 and 19 to stack
+    ; ---------------- 
+    sub esp, 2
+    mov word[esp], 23
+    sub esp, 2
+    mov word[esp], 19
+    ; ---------------- 
+    ; POP AND ADD
+    ; ---------------- 
+    xor eax, eax
+    add ax, word[esp]
+    add esp, 2
+    add ax, word[esp]
+    add esp, 2
+
+    ; ---------------- 
+    ; WRITE TO BUFFER 
+    ; ---------------- 
+
+    xor edx, edx
+    mov ecx, 10
+    div ecx
+    add edx, '0'
+    add eax, '0'
+    mov [answer], al
+    mov [answer + 1], dl
+
+    ; ---------------- 
+    ; PRINT AND EXIT
+    ; ---------------- 
+
     mov eax, 4                          ; write
     mov ebx, 1                          ; stdout
     mov ecx, message
