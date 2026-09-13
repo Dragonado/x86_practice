caeb7722287e6d63692d8bed25df0ef128885d2b
Dragonado <29285950+Dragonado@users.noreply.github.com>
2026-09-12 20:12:40 -0400

    Add functipon solution 7

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,12 @@ RUN dpkg --add-architecture i386 && \
     apt-get update && \
     apt-get install -y --no-install-recommends \
       nasm binutils gcc-multilib gdb make libc6-dev-i386 python3 qemu-user \
+      ca-certificates curl ncurses-term xz-utils \
     && rm -rf /var/lib/apt/lists/*
+
+RUN curl --proto '=https' --tlsv1.2 -LsSf https://install.pwndbg.re \
+    | sh -s -- -t pwndbg-gdb && \
+    ln -s /usr/local/bin/pwndbg /usr/local/bin/gdb-pwndbg
 
 WORKDIR /workspace
 CMD ["/bin/bash"]
--- src/07-functions/starter.asm
+++ src/07-functions/starter.asm
@@ -1,12 +1,46 @@
 global _start
 
 section .data
-    message      db   'TODO: implement this module', 10
+    message      db   'function result: '
+    answer       db   '00'
     message_len  equ  $ - message
 
 section .text
 
+function_add:
+    pop ecx                             ;  save return address
+    xor eax, eax
+    xor ebx, ebx
+    pop eax
+    pop ebx
+    add eax, ebx
+    push ecx
+    ret
+
 _start:
+    ; ---------------- 
+    ; PUSH 23 and 19 to stack
+    ; ---------------- 
+    push 23
+    push 19
+    call function_add
+
+    ; ---------------- 
+    ; WRITE TO BUFFER 
+    ; ---------------- 
+
+    xor edx, edx
+    mov ecx, 10
+    div ecx
+    add edx, '0'
+    add eax, '0'
+    mov [answer], al
+    mov [answer + 1], dl
+
+    ; ---------------- 
+    ; PRINT AND EXIT
+    ; ---------------- 
+
     mov eax, 4                          ; write
     mov ebx, 1                          ; stdout
     mov ecx, message
